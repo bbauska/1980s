47eb679d340e2cf9c36ba1396668444578bfa367
Brian Bauska <brianbauska@gmail.com>
2025-08-14 09:09:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/** js/scripts.js of bbauska/1990s making 1990s.bauska.org */
+/** js/scripts.js of bbauska/1980s making 1980s.bauska.org */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
